7f6f4f7ee2cbd24f9b18a99ef5b9b69b5a35cec7
Nicholas Outram <njoutram@users.noreply.github.com>
2025-01-24 17:40:58 +0000

    Fixed to a good size

--- wx/wxSimpleNoteTaker/MyProjectBase.py
+++ wx/wxSimpleNoteTaker/MyProjectBase.py
@@ -22,30 +22,22 @@ class MainDialog ( wx.Dialog ):
     def __init__( self, parent ):
         wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 270,303 ), style = wx.DEFAULT_DIALOG_STYLE )
 
-        self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+        self.SetSizeHints( wx.Size( 600,600 ), wx.DefaultSize )
 
         verticalSizer = wx.BoxSizer( wx.VERTICAL )
 
-        self.btnOk = wx.Button( self, wx.ID_ANY, _(u"OK"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.btnOk = wx.Button( self, wx.ID_ANY, _(u"Save"), wx.DefaultPosition, wx.DefaultSize, 0 )
         self.btnOk.SetMinSize( wx.Size( 100,-1 ) )
 
         verticalSizer.Add( self.btnOk, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
-
-        self.btnSave = wx.Button( self, wx.ID_ANY, _(u"Save"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.btnSave.SetToolTip( _(u"Click this to edit") )
-        self.btnSave.SetMinSize( wx.Size( 100,-1 ) )
-
-        verticalSizer.Add( self.btnSave, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
-
-        self.txtEdit = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE )
-        self.txtEdit.SetMinSize( wx.Size( 300,-1 ) )
-
-        verticalSizer.Add( self.txtEdit, 1, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
 
         self.lblMessage = wx.StaticText( self, wx.ID_ANY, _(u"Enter notes"), wx.DefaultPosition, wx.DefaultSize, 0 )
         self.lblMessage.Wrap( -1 )
 
         verticalSizer.Add( self.lblMessage, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+        self.txtEdit = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        verticalSizer.Add( self.txtEdit, 1, wx.ALL|wx.EXPAND, 5 )
 
 
         self.SetSizer( verticalSizer )
@@ -55,7 +47,6 @@ class MainDialog ( wx.Dialog ):
 
         # Connect Events
         self.btnOk.Bind( wx.EVT_LEFT_DOWN, self.onOkClicked )
-        self.btnSave.Bind( wx.EVT_LEFT_DOWN, self.onEditClicked )
 
     def __del__( self ):
         pass
@@ -63,9 +54,6 @@ class MainDialog ( wx.Dialog ):
 
     # Virtual event handlers, override them in your derived class
     def onOkClicked( self, event ):
-        event.Skip()
-
-    def onEditClicked( self, event ):
         event.Skip()
 
 
